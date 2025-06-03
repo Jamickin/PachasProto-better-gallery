3739ed88b87eae9484184c9ec725fb05e2f13a6f
James <jamickina@gmail.com>
2025-06-03 19:24:01 +0200

    Layout update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,3 @@
-<script type="application/ld+json">
-{
-  "@context": "https://schema.org",
-  "@type": "Restaurant",
-  "name": "Pachas Restaurant",
-  "image": "https://pachas.co.za/pachas_logo.jpg",
-  "telephone": "+27124605063",
-  "address": {
-    "@type": "PostalAddress",
-    "streetAddress": "22 Dely Road, The Club Retail",
-    "addressLocality": "Hazelwood",
-    "addressRegion": "Gauteng",
-    "postalCode": "0181",
-    "addressCountry": "ZA"
-  },
-  "geo": {
-    "@type": "GeoCoordinates",
-    "latitude": -25.778474,
-    "longitude": 28.254576
-  },
-  "url": "https://pachas.co.za",
-  "servesCuisine": ["South African", "Steakhouse", "Seafood"],
-  "priceRange": "$$$$",
-  "openingHours": "Mo-Sa 12:00-22:00"
-}
-</script>
-
 <script>
 	import Sidebar from '$lib/components/Sidebar.svelte';
 	import Topbar from '$lib/components/Topbar.svelte';
@@ -33,7 +6,34 @@
 	let { children } = $props();
 </script>
 
-
+<svelte:head>
+	<script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@type": "Restaurant",
+			"name": "Pachas Restaurant",
+			"image": "https://pachas.co.za/pachas_logo.jpg",
+			"telephone": "+27124605063",
+			"address": {
+				"@type": "PostalAddress",
+				"streetAddress": "22 Dely Road, The Club Retail",
+				"addressLocality": "Hazelwood",
+				"addressRegion": "Gauteng",
+				"postalCode": "0181",
+				"addressCountry": "ZA"
+			},
+			"geo": {
+				"@type": "GeoCoordinates",
+				"latitude": -25.778474,
+				"longitude": 28.254576
+			},
+			"url": "https://pachas.co.za",
+			"servesCuisine": ["South African", "Steakhouse", "Seafood"],
+			"priceRange": "$$$$",
+			"openingHours": "Mo-Sa 12:00-22:00"
+		}
+	</script>
+</svelte:head>
 
 <header class="max-md:hidden"><Topbar></Topbar></header>
 <header class="min-md:hidden">
@@ -48,5 +48,3 @@
 <main class="mt-68 mb-24 max-md:{'mt-12'} max-md:{'px-6'} px-8">
 	{@render children()}
 </main>
-
-<style></style>
